6e0b06f64ec7d3f55f347b039717058db58957bf
Nils Erickson <nils.o.erickson@gmail.com>
2017-05-24 22:29:48 -0700

    Initialize Unity Upload
    This is the project as it stands. You can find a playable version of
    this on Newgrounds (links to follow).

---
+++ Assets/Standard Assets/2D/Scripts/Completelv2A.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+	public class Completelv2A : MonoBehaviour
+	{
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.tag == "Player")
+			{
+				Application.LoadLevel ("Level 3A");
+			}
+		}
+	}
+}
---
+++ Assets/Standard Assets/2D/Scripts/Completelv3B.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+	public class Completelv3B : MonoBehaviour
+	{
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.tag == "Player")
+			{
+				Application.LoadLevel ("Level 4");
+			}
+		}
+	}
+}
